182d7832e79e0b60f32959f6dc7677243d4c5ff3
Samuel Klumpers <samuelhklumpers@gmail.com>
2019-01-26 16:38:44 +0100

    use packing and differential packing

--- wtc/wtc.py
+++ wtc/wtc.py
@@ -1,17 +1,143 @@
 import lzma
 import struct
 
+import numpy as np
+
+escape = -2 ** 7
+
+def unsorted_diff_pack_16_8(int16s):
+    """Packs the differential of the input to bytes in little endian order."""
+    
+    start = int16s[0]
+    diff = np.diff(int16s)
+    small = 2 ** 7 - 1
+    
+    packed = []
+    
+    def pack(word):
+        if abs(word) <= small:
+            packed.append(word)
+        else:
+            packed.append(escape)
+            packed.append(word & 0xFF)
+            word = word >> 8
+            packed.append(word)
+    
+    pack(start)
+    
+    for word in diff:
+        pack(word)
+            
+    return np.int8(packed)
+
+def unsorted_diff_unpack_8_16(ints8):
+    """Unpacks the differential data in little endian order to words."""
+    decoded = []
+    i = 0
+    while i < len(ints8):
+        byte = ints8[i]
+        
+        if byte == escape:
+            i += 1
+            word = ints8[i] & 0xFF
+            i += 1
+            word += ints8[i] << 8
+            decoded.append(word)
+        else:
+            decoded.append(byte)
+        
+        i += 1
+        
+    decompressed = np.cumsum(decoded)
+    
+    return np.int16(decompressed)
+
+def pack_32_8(ints32):
+    """Packs the data in little endian order."""
+    packed = []
+    small = 2 ** 7 - 1
+    
+    for dword in ints32:
+        if abs(dword) <= small:
+            packed.append(dword)
+        else:
+            packed.append(escape)
+            packed.append(dword & 0xFF)
+            dword = dword >> 8
+            packed.append(dword & 0xFF)
+            dword = dword >> 8
+            packed.append(dword & 0xFF)
+            dword = dword >> 8
+            packed.append(dword)
+            
+    return np.int8(packed)
+
+def unpack_8_32(ints8):
+    """Unpacks the data in little endian order."""
+    unpacked = []
+    i = 0
+    while i < len(ints8):
+        byte = ints8[i]
+        
+        if byte == escape:
+            i += 1
+            dword = ints8[i] & 0xFF
+            i += 1
+            dword += (ints8[i] << 8) & 0xFF00
+            i += 1
+            dword += (ints8[i] << 16) & 0xFF0000
+            i += 1
+            dword += ints8[i] << 24
+            unpacked.append(dword)
+        else:
+            unpacked.append(byte)
+            
+        i += 1
+            
+    return np.int32(unpacked)
+
 def compress(lzma_stream):
-    """
-    Packs replay into a more compact format.
 
-    Args:
-        String lzma_stream: An lzma stream from a replay.
+    #separate the lzma stream to apply different compression for each datatype
+    xs, ys, zs, ws = separate(lzma_stream)
 
-    Returns:
-        An lzma compressed bytestring
-    """
+    xs = unsorted_diff_pack_16_8(xs)
+    ys = unsorted_diff_pack_16_8(ys)
 
+    ws = pack_32_8(ints32)
+
+    #store all data as arrays of bytes with their lenght stored in the first 4 bytes
+    def pack_bytes(bs):
+        return struct.pack(f'<I{len(bs)}B', len(bs), *bs)
+
+    buf = b''.join([pack_bytes(bs) for bs in (xs, ys, zs, ws)])
+
+    return lzma.compress(raw, format=2)
+
+def decompress(lzma_stream):
+    data = lzma.decompress(lzma_stream)
+
+    def unpack_bytes(data):
+        size = struct.unpack('<I', data[:4])
+        data = data[4:]
+        bs = struct.unpack(f'<{size}B', data[:size])
+        data = data[size:]
+
+        return bs, data
+
+    xs, data = unpack_bytes(data)
+    ys, data = unpack_bytes(data)
+    zs, data = unpack_bytes(data)
+    ws, data = unpack_bytes(data)
+
+    xs = unsorted_diff_unpack_8_16(xs)
+    ys = unsorted_diff_unpack_8_16(ys)
+    
+    ws = unpack_8_32(ws)
+
+    return combine(xs, ys, zs, ws)
+
+def separate(lzma_stream):
     text = lzma.decompress(lzma_stream).decode('UTF-8')
     raw_list = []
     for frame in text.split(','):
@@ -40,83 +166,24 @@ def compress(lzma_stream):
         #x: signed short
         #y: signed short
         #z: unsigned char
-        raw_list.append(_pack_int24(w)) #w
-        raw_list.append(struct.pack('<hhB', x, y, z)) #x, y, and z
+        xs.append(x)
+        ys.append(y)
+        zs.append(z)
+        ws.append(w)
 
-    raw = b''.join(raw_list)
-    compressed = lzma.compress(raw, format=2)
-    return compressed
+    return xs, ys, zs, ws
 
-def decompress(wtc_bytes):
-    """
-    Decompresses a wtc stream into an lzma stream.
+def combine(xs, ys, zs, ws):
+    if not len(xs) == len(ys) == len(zs) == len(ws):
+        raise ValueError("The bytearrays are of unequal lengths")
 
-    Args:
-        String wtc_bytes: A wtc compressed bytestring (returned by wtc.compress).
+    xs = np.array(xs) / 16
+    ys = np.array(ys) / 16
 
-    Returns:
-        An lzma compressed bytestring, identical to the (decoded) string returned by the get_replay api endpoint.
-    """
-    output_list = []
-    data = lzma.decompress(wtc_bytes)
-    #each frame is 8 bytes
-    for i in range(0, len(data), 8):
-        frame = data[i : i+8]
-        #extract W on its own since it's an int24 and cannot be used with struct.unpack
-        b_w, frame = frame[:3], frame[3:]
+    frames = zip(xs, ys, zs, ws)
 
-        #w: signed 24bit integer
-        w = _unpack_int24(b_w)
+    frames = [f'{w}|{x}|{y}|{z},' for x, y, z, w in frames]
 
-        #x: signed short
-        #y: signed short
-        #z: unsigned char
-        x, y, z = struct.unpack('<hhB', frame)
+    output = ''.join(frames)
 
-        #X and Y are stored as shorts; convert and scale them back to their float forms
-        x /= 16
-        y /= 16
-
-        output_list.append(f'{w}|{x}|{y}|{z},')
-
-    output = ''.join(output_list)
-    lzma_stream = lzma.compress(output.encode('UTF-8'), format=2)
-    return lzma_stream
-
-
-def _pack_int24(integer):
-    """
-    Converts an integer to a 24 bit bytes object.
-
-    Args:
-        Integer integer: The number to convert to bytes.
-
-    Returns:
-        A 24 bit int as bytes.
-    """
-
-    if not (-0x800000 <= integer < 0x800000):
-        raise ValueError('Value must be between -0x800000 and 0x800000')
-    output = struct.pack('<i', integer)
-    output = output[:-1]
-    return output
-
-def _unpack_int24(int_bytes):
-    """
-    Converts a 24 bit bytes object to an integer.
-
-    Args:
-        Bytes int_bytes: The bytes to convert to int.
-
-    Returns:
-        An integer representation of the byte input.
-    """
-
-    if len(int_bytes) != 3:
-        raise ValueError('Value must be an int24')
-    sign = int_bytes[-1] & 0x80
-    if sign:
-        int_bytes = int_bytes + b'\xFF'
-    else:
-        int_bytes = int_bytes + b'\x00'
-    return struct.unpack('<i', int_bytes)[0]
+    return lzma.compress(output.encode('UTF-8'), format=2)
