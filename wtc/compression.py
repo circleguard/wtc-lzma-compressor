f6cc2773dd2da16083ef3e337960378a835baef2
Samuel Klumpers <samuelhklumpers@gmail.com>
2019-01-27 13:11:01 +0100

    implement sparse clusterdata compression

--- wtc/compression.py
+++ wtc/compression.py
@@ -2,6 +2,8 @@ import struct
 
 import numpy as np
 
+from itertools import zip_longest
+
 def pack_word_byte(word):
     if abs(word) < 128:
         return np.int8((word,))
@@ -115,3 +117,82 @@ def unpack_8_32(ints8):
         i += 1
             
     return np.int32(unpacked)
+
+def scolvr(sparse):
+    """
+    Sparse clustered data to offset, length and value reduction.
+
+    Args:
+        List sparse: The sparse data to compress.
+
+    Returns:
+        The list of short data formatted as offset, length, value.
+    """
+
+    marker = 0
+    offset = 0
+    length = 0
+    current = 0
+
+    shorts = []
+
+    for i, e in enumerate(sparse):
+        if current > 0:
+            if e == current:
+                length += 1
+            else:
+                #non-zero to non-zero transition
+                #also non-zero to zero transition but zero isn't tracked
+                shorts.append(offset)
+                shorts.append(length)
+                shorts.append(current)
+                
+                offset = 0
+                marker = i
+                length = 1
+                current = e
+        elif e > 0:
+            #zero to non-zero transition
+            offset = i - marker
+            length = 1
+            current = e
+
+    if current > 0:
+        shorts.append(offset)
+        shorts.append(length)
+        shorts.append(current)
+        
+    return shorts
+
+def grouper(iterable, n, fillvalue=None):
+    #https://docs.python.org/3/library/itertools.html#itertools-recipes
+
+    #duplicate the iterable n times
+    args = [iter(iterable)] * n
+    #zip the same iterable together n times
+    #taking the 1st element removes it from the iterable
+    #taking the 1st again now takes actually the 2nd from the original, up to n
+    return zip_longest(*args, fillvalue=fillvalue)
+
+def olvsco(olv_data):
+    """
+    Decompresses the offset, length and value data to sparse data.
+
+    Args:
+        List olv_data: The short data of the format offset, length, value.
+
+    Returns:
+        The sparse data represented by olv_data.
+    """
+
+    if len(olv_data) % 3:
+        raise ValueError(f"Offset, length, value data length is not a multiple of 3. Length: {len(ilv_data)}")
+
+    last = 0
+    sparse = []
+    
+    for o, l, v in grouper(olv_data, 3):
+        sparse.extend(o * [0])
+        sparse.extend(l * [v])
+
+    return sparse
