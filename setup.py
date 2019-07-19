a93b6f79cbb699d150ebf52184dc78923a4ff0bf
tybug <orionldevoe@gmail.com>
2019-07-19 18:22:49 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
-import setuptools
+from distutils.core import setup
+from setuptools import find_packages
+from wtc.__init__ import __version__
 
 with open("README.md", "r", encoding="utf8") as readme:
     long_description = readme.read()
 
-setuptools.setup(
-    name="wtc",
-    version="1.1.3",
-    author="Chris Miuchiz",
-    author_email="chrismiuchiz@gmail.com",
-    description="Compress osu replay lzma bytestrings",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/osu-anticheat/wtc-lzma-compressor",
-    packages=setuptools.find_packages(),
-    classifiers=[
+setup(
+    name = "wtc",
+    version = __version__,
+    description = "Compress osu replay lzma bytestrings",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    keywords = ["osu!, compression, lzma, python, replay, osr"],
+    author = "Chris Miuchiz",
+    author_email = "chrismiuchiz@gmail.com",
+    url = "https://github.com/circleguard/wtc-lzma-compressor",
+    download_url = "https://github.com/circleguard/wtc-lzma-compressor/tarball/" + __version__,
+    license = "MIT",
+    packages = find_packages()
 )
--- wtc/__init__.py
+++ wtc/__init__.py
@@ -1,1 +1,3 @@
 from .wtc import compress, decompress
+
+__version__ = "1.1.3"
